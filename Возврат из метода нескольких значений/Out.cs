e60c66d34627a18f3c50da3dc423d2bb55709f86
BattleSky <1battlesky@gmail.com>
2020-09-11 16:42:47 +0300

    Добавлено: Возврат из метода нескольких значений

---
+++ Возврат из метода нескольких значений/Out.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Возврат_из_метода_нескольких_значений
+{
+    /* При помощи ref и out можно объявить несколько возвращаемых методом полей
+     Но ref устанавливает двусторонее соответствие: можно изменить значение до входа в метод и 
+    этим повлиять на работу метода. 
+    Out работает в одностороннем порядке, надо указать, что мы хотим получить
+    Без инициализации поля не получится получить значение из метода. При наличии инструкции out
+    */
+    class Out
+    {
+        static Random rnd = new Random();
+
+        public static bool GetNumber3(ref int value)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (Console.KeyAvailable)
+                {
+                    value = rnd.Next(100);
+                    return true;
+                }
+                Thread.Sleep(100);
+            }
+            return false;
+        }
+
+        static void Main3()
+        {
+            int value = 0;
+            if (!GetNumber3(ref value))
+                Console.WriteLine("Error");
+            else
+                Console.WriteLine(value);
+        }
+
+        public static bool GetNumber4(out int value)
+        {
+            //Console.WriteLine(value); // value не может быть использовано до присвоения внутри метода
+            for (int i = 0; i < 10; i++)
+            {
+                if (Console.KeyAvailable)
+                {
+                    value = rnd.Next(100);
+                    return true;
+                }
+                Thread.Sleep(100);
+            }
+            //value обязано быть присвоено до выхода из метода
+            value = 0;
+            return false;
+        }
+
+        static void Main4()
+        {
+            int value;
+            if (!GetNumber4(out value))
+                Console.WriteLine("Error");
+            else
+                Console.WriteLine(value);
+        }
+    }
+}
---
+++ Возврат из метода нескольких значений/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Возврат из метода нескольких значений")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Возврат из метода нескольких значений")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("53d2d999-22f8-4362-af3e-d9f0aa9505c6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
