5a57edcda55e6d6e84b12532ac4584d60a9b0c41
BattleSky <1battlesky@gmail.com>
2020-09-17 08:50:32 +0300

    tempResult (not working)

---
+++ ClonesExcercise/CloneVersionSystem.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Clones
+{
+	public class CloneVersionSystem : ICloneVersionSystem
+    {
+
+        public class CloneClass
+        {
+            public string Index;
+            public string CurrentProgram;
+            public LinkedList<string> ProgramToRollback;
+            public LinkedList<string> ProgramToRelearn;
+        }
+
+        private List<CloneClass> Clones = new List<CloneClass>() 
+        {new CloneClass() 
+            {
+                Index = "1", 
+                CurrentProgram = "basic", 
+                ProgramToRelearn = new LinkedList<string>(), 
+                ProgramToRollback = new LinkedList<string>()
+            }
+        };
+
+        public string Execute(string query)
+        {
+            var splitQuery = query.Split(' ');
+            // ужасно с точки зрения ошибок,
+            // но условия задачи уверяют, что все команды корректны
+            var clone = FindCloneByIndex(splitQuery[1]); 
+            switch (splitQuery[0])
+            {
+                case "learn":
+                    Learn(clone, splitQuery[2]);
+                    break;
+                case "rollback":
+                    Rollback(clone);
+                    break;
+                case "relearn":
+                    Relearn(clone);
+                    break;
+                case "clone":
+                    Clone(clone);
+                    break;
+                case "check":
+                    return Check(clone);
+            }
+            return null;
+        }
+        
+        private CloneClass FindCloneByIndex(string index)
+        {
+            var clone = Clones[Convert.ToInt32(index) - 1];
+            return clone;
+        }
+
+        private void Learn(CloneClass clone, string program)
+        {
+            clone.ProgramToRollback.AddLast(clone.CurrentProgram);
+            clone.CurrentProgram = program;
+            clone.ProgramToRelearn.Clear();;
+        }
+        private void Rollback(CloneClass clone)
+        {
+            clone.ProgramToRelearn.AddLast(clone.CurrentProgram);
+            clone.CurrentProgram = clone.ProgramToRollback.Last.Value;
+            clone.ProgramToRollback.RemoveLast();
+        }
+        private void Relearn(CloneClass clone)
+        {
+            clone.CurrentProgram = clone.ProgramToRelearn.Last.Value;
+        }
+        private void Clone(CloneClass oldClone)
+        {
+            //Не ковертируй стринг в инт без проверки . . . . .
+            //но тут можно потому что все команды корректны по условию задачи
+            //ВСЕДОЗВОЛЕННОСТЬ, АНАРХИЯ, МУХАХАХАХАХА
+            var newIndex = Convert.ToInt32(Clones.Count) + 1;
+            var newClone = new CloneClass()
+            {
+                Index = newIndex.ToString(),
+                CurrentProgram = oldClone.CurrentProgram,
+                ProgramToRelearn = new LinkedList<string>(),
+                ProgramToRollback = new LinkedList<string>()
+            };
+            if (oldClone.ProgramToRelearn.First != null)
+                newClone.ProgramToRelearn.AddFirst(oldClone.ProgramToRelearn.First);
+            if (oldClone.ProgramToRollback.First != null)
+                newClone.ProgramToRollback.AddFirst(oldClone.ProgramToRollback.First);
+            Clones.Add(newClone);
+        }
+        private string Check(CloneClass clone)
+        {
+            return clone.CurrentProgram;
+        }
+    }
+}
---
+++ ClonesExcercise/CloneVersionSystem_should.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Clones
+{
+	[TestFixture]
+	public class CloneVersionSystem_should
+	{
+		[Test]
+		public void Learn()
+		{
+			var clone1 = Execute("learn 1 45", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+		[Test]
+		public void RollbackToBasic()
+		{
+			var clone1 = Execute("learn 1 45", "rollback 1", "check 1").Single();
+			Assert.AreEqual("basic", clone1);
+		}
+
+		[Test]
+		public void RollbackToPreviousProgram()
+		{
+			var clone1 = Execute("learn 1 45", "learn 1 100500", "rollback 1", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+        [Test]
+        public void RelearnAfterDoubleRollbacksAtSecondClone()
+        {
+            var clone1 = Execute("learn 1 10", "learn 1 20 ", "learn 1 30", "clone 1", "rollback 2", "rollback 2", "relearn 2", "check 2").Single();
+			Assert.AreEqual("20", clone1);
+        }
+
+        [Test]
+		public void RelearnAfterRollback()
+		{
+			var clone1 = Execute("learn 1 45", "rollback 1", "relearn 1", "check 1").Single();
+			Assert.AreEqual("45", clone1);
+		}
+
+        [Test]
+		public void CloneBasic()
+		{
+			var clone2 = Execute("clone 1", "check 2").Single();
+			Assert.AreEqual("basic", clone2);
+		}
+
+		[Test]
+		public void CloneLearned()
+		{
+			var clone2 = Execute("learn 1 42", "clone 1", "check 2").Single();
+			Assert.AreEqual("42", clone2);
+		}
+
+		[Test]
+		public void LearnClone_DontChangeOriginal()
+		{
+			List<string> res = Execute("learn 1 42", "clone 1", "learn 2 100500", "check 1", "check 2");
+			Assert.AreEqual(new []{ "42", "100500"}, res);
+		}
+
+		[Test]
+		public void RollbackClone_DontChangeOriginal()
+		{
+			var res = Execute("learn 1 42", "clone 1", "rollback 2", "check 1", "check 2");
+			Assert.AreEqual(new[] { "42", "basic" }, res);
+		}
+
+		[Test]
+		public void ExecuteSample()
+		{
+			var res = Execute("learn 1 5",
+				"learn 1 7",
+				"rollback 1",
+				"check 1",
+				"clone 1",
+				"relearn 2",
+				"check 2",
+				"rollback 1",
+				"check 1");
+			Assert.AreEqual(new[] { "5", "7", "basic" }, res);
+		}
+
+        [Test]
+        public void ThreeClonesTest()
+        {
+			var res = Execute("learn 1 10", "clone 1", "learn 2 20", "clone 1", "learn 3 20", "learn 3 30", "rollback 3", "rollback 3", "learn 3 30", "check 1", "check 2", "check 3" );
+			Assert.AreEqual(new[] {"10", "20", "30"}, res);
+        }
+        [Test]
+        public void TenClonesTest()
+        {
+            var res = Execute(
+                "learn 1 10", "clone 1", //2
+                "learn 2 20", "clone 1", //3
+                "learn 3 20", "learn 3 30",
+                "rollback 3", "rollback 3",
+                "learn 3 30",
+                "clone 3", "clone 3", //4,5
+                "learn 5 50",
+                "rollback 5", "relearn 5",
+                "clone 5", "clone 5", // 6,7
+                "learn 5 60", "clone 5", //8
+                "rollback 8", "learn 6 60", "learn 7 70", "learn 8 80",
+                "clone 8", "clone 8", "rollback 10", "rollback 9", "relearn 10",// 9,10
+                "check 1", "check 2", "check 3", "check 4", "check 5", "check 6", "check 7", "check 8", "check 9", "check 10"); 
+            Assert.AreEqual(new[] { "10", "20", "30", "30", "60", "60", "70", "80", "50", "80"}, res);
+        }//									1	  2     3     4     5	  6	    7      8     9     10
+
+
+
+		private List<string> Execute(params string[] queries)
+		{
+			var cvs = Factory.CreateCVS();
+			var results = new List<string>();
+			foreach (var command in queries)
+			{
+				var result = cvs.Execute(command);
+				if (result != null) results.Add(result);
+			}
+			return results;
+		}
+	}
+}
