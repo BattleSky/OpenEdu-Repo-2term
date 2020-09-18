1f9c0f9eed96111ed5088311b22903d72642c300
BattleSky <1battlesky@gmail.com>
2020-09-18 12:07:23 +0300

    cleanup code by resharper

--- ClonesExcercise/CloneVersionSystem.cs
+++ ClonesExcercise/CloneVersionSystem.cs
@@ -1,95 +1,17 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Runtime.ExceptionServices;
-using System.Text;
-using NUnit.Framework.Internal;
 
 namespace Clones
 {
-
-
     public class CloneVersionSystem : ICloneVersionSystem
     {
-     //   public static StringBuilder log = new StringBuilder();
-        // Элемент хранит значение и ссылку на предыдущий
-        // Реализация односвязного списка
-        public class Programms<T>
-        { 
-            public Programms<T> PreviousNode { get; set; }
-            public T Value { get; set; }
-        }
-
-
-        // Стэк хранит значения последнего и размер себя;
-        public class ProgrammStack<T>
+        private readonly List<Droid> Clones = new List<Droid>
         {
-            public Programms<T> Last;
-            public int count = 0;
-            // Last - последний в стеке, первый на выход
-            // [[stack]] : [1] <- [2] <- [3]  <= push
-
-            public T Pop()
+            new Droid
             {
-                var result = Last;
-                if (result == null)
-                    throw new InvalidOperationException("Last is null with counter=" + count + " log: \n");
-                Last = result.PreviousNode;
-                count--;
-                return result.Value;
-            }
-            public T Peek()
-            {
-                try
-                {
-                    var result = Last;
-                    if (result == null)
-                        return default;
-                }
-                catch
-                {
-                    return default;
-                }
-                return Last.Value;
-            }
-
-            public void Push(T item)
-            {
-                if (Last == null) //очередь пуста, значит последний -  null
-                    Last = new Programms<T>() { Value = item };
-                else
-                {
-                    var newItem = new Programms<T>() { Value = item, PreviousNode = Last};
-                    Last = newItem;
-                }
-                count++;
-                // последний используется, чтобы положить в стек новый элемент
-                // и сначала сослаться на старый, а потом переприсвоить first как новый
-            }
-
-            // в этом случае Last обнуляет ссылку на последний элемент
-            // получается, что ничто не указывает на элементы в стеке
-            // сборщик мусора их увезет
-            public void Clear()
-            {
-                Last = null;
-                count = 0;
-            }
-
-        }
-        public class Droid
-        {
-            public string CurrentProgram;
-            public ProgrammStack<string> ProgramToRollback;
-            public ProgrammStack<string> ProgramToRelearn;
-        }
-
-        private List<Droid> Clones = new List<Droid>() 
-        {new Droid() 
-            {
-                CurrentProgram = "basic", 
-                ProgramToRelearn = new ProgrammStack<string>(), 
-                ProgramToRollback = new ProgrammStack<string>() 
+                CurrentProgram = "basic",
+                ProgramToRelearn = new ProgrammStack<string>(),
+                ProgramToRollback = new ProgrammStack<string>()
             }
         };
 
@@ -99,7 +21,7 @@ namespace Clones
             var splitQuery = query.Split(' ');
             // ужасно с точки зрения ошибок,
             // но условия задачи уверяют, что все команды корректны
-            var clone = FindCloneByIndex(splitQuery[1]); 
+            var clone = FindCloneByIndex(splitQuery[1]);
             switch (splitQuery[0])
             {
                 case "learn":
@@ -115,12 +37,13 @@ namespace Clones
                     Clone(clone);
                     break;
                 case "check":
-                  //  log.Append("Check \n\n");
+                    //  log.Append("Check \n\n");
                     return Check(clone);
             }
+
             return null;
         }
-        
+
         private Droid FindCloneByIndex(string index)
         {
             var clone = Clones[Convert.ToInt32(index) - 1];
@@ -135,16 +58,18 @@ namespace Clones
             clone.CurrentProgram = program;
             clone.ProgramToRelearn.Clear();
             //log.Append("Current: " + clone.CurrentProgram + " ProgramToRelearn: " + clone.ProgramToRelearn.Peek() + " ProgramToRollback: " +
-                       //clone.ProgramToRollback.Peek() + "\n\n");
+            //clone.ProgramToRollback.Peek() + "\n\n");
         }
+
         private void Rollback(Droid clone)
         {
             //log.Append("* Rollback from " + clone.CurrentProgram + " to " + clone.ProgramToRollback.Peek() + "\n");
             clone.ProgramToRelearn.Push(clone.CurrentProgram);
             clone.CurrentProgram = clone.ProgramToRollback.Pop();
             //log.Append("* Current: " + clone.CurrentProgram + " ProgramToRelearn: " + clone.ProgramToRelearn.Peek() + " ProgramToRollback: " +
-                       //clone.ProgramToRollback.Peek() + "\n\n");
+            //clone.ProgramToRollback.Peek() + "\n\n");
         }
+
         private void Relearn(Droid clone)
         {
             //log.Append("** Relearn from " + clone.CurrentProgram + " to " + clone.ProgramToRelearn.Peek() + "\n");
@@ -153,26 +78,108 @@ namespace Clones
             //log.Append("** Current: " + clone.CurrentProgram + " ProgramToRelearn: " + clone.ProgramToRelearn.Peek() + " ProgramToRollback: " +
             //           clone.ProgramToRollback.Peek() + "\n\n");
         }
+
         private void Clone(Droid oldClone)
         {
             //Не ковертируй стринг в инт без проверки . . . . .
             //но тут можно потому что все команды корректны по условию задачи
             //ВСЕДОЗВОЛЕННОСТЬ, АНАРХИЯ, МУХАХАХАХАХА
-            var newClone = new Droid()
+            var newClone = new Droid
             {
                 CurrentProgram = oldClone.CurrentProgram,
-                ProgramToRelearn = new ProgrammStack<string>(){Last = oldClone.ProgramToRelearn.Last, count = oldClone.ProgramToRelearn.count},
-                ProgramToRollback = new ProgrammStack<string>(){ Last = oldClone.ProgramToRollback.Last, count = oldClone.ProgramToRollback.count}
+                ProgramToRelearn = new ProgrammStack<string>
+                    {Last = oldClone.ProgramToRelearn.Last, count = oldClone.ProgramToRelearn.count},
+                ProgramToRollback = new ProgrammStack<string>
+                    {Last = oldClone.ProgramToRollback.Last, count = oldClone.ProgramToRollback.count}
             };
             //log.Append("*** Clone\n*** old: Current: " + oldClone.CurrentProgram + " ProgramToRelearn: " +oldClone.ProgramToRelearn.Peek() + " ProgramToRollback: " +
             //           oldClone.ProgramToRollback.Peek() + "\n" + "*** new "+ Clones.Count + ": Current: " + newClone.CurrentProgram + " ProgramToRelearn: " + newClone.ProgramToRelearn.Peek() + " ProgramToRollback: " +
             //           newClone.ProgramToRollback.Peek() + "\n\n");
             Clones.Add(newClone);
-            
         }
+
         private string Check(Droid clone)
         {
             return clone.CurrentProgram;
+        }
+
+        //   public static StringBuilder log = new StringBuilder();
+        // Элемент хранит значение и ссылку на предыдущий
+        // Реализация односвязного списка
+        public class Programms<T>
+        {
+            public Programms<T> PreviousNode { get; set; }
+            public T Value { get; set; }
+        }
+
+
+        // Стэк хранит значения последнего и размер себя;
+        public class ProgrammStack<T>
+        {
+            public int count;
+
+            public Programms<T> Last;
+            // Last - последний в стеке, первый на выход
+            // [[stack]] : [1] <- [2] <- [3]  <= push
+
+            public T Pop()
+            {
+                var result = Last;
+                if (result == null)
+                    throw new InvalidOperationException("Last is null with counter=" + count + " log: \n");
+                Last = result.PreviousNode;
+                count--;
+                return result.Value;
+            }
+
+            public T Peek()
+            {
+                try
+                {
+                    var result = Last;
+                    if (result == null)
+                        return default;
+                }
+                catch
+                {
+                    return default;
+                }
+
+                return Last.Value;
+            }
+
+            public void Push(T item)
+            {
+                if (Last == null) //очередь пуста, значит последний -  null
+                {
+                    Last = new Programms<T> {Value = item};
+                }
+                else
+                {
+                    var newItem = new Programms<T> {Value = item, PreviousNode = Last};
+                    Last = newItem;
+                }
+
+                count++;
+                // последний используется, чтобы положить в стек новый элемент
+                // и сначала сослаться на старый, а потом переприсвоить first как новый
+            }
+
+            // в этом случае Last обнуляет ссылку на последний элемент
+            // получается, что ничто не указывает на элементы в стеке
+            // сборщик мусора их увезет
+            public void Clear()
+            {
+                Last = null;
+                count = 0;
+            }
+        }
+
+        public class Droid
+        {
+            public string CurrentProgram;
+            public ProgrammStack<string> ProgramToRelearn;
+            public ProgrammStack<string> ProgramToRollback;
         }
     }
 }
