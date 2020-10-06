6e452ebf46655ce982155e8c7bd2669a00f5fbbb
BattleSky <1battlesky@gmail.com>
2020-10-06 12:15:51 +0300

    ghosts practice

---
+++ ghosts/GhostsTask.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Reflection;
+using System.Text;
+
+namespace hashes
+{
+	public class GhostsTask : 
+		IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, 
+		IMagic
+    {
+        private static byte[] massiveBytes = {1, 2, 3};
+        private static int justANumber = 27;
+        private Document document = new Document("firstDoc", Encoding.UTF8, massiveBytes);
+        private Vector vector = new Vector(5, 6);
+        private Segment segment = new Segment(new Vector(1, 1), new Vector(2, 2));
+        private Cat blackCat = new Cat("Flawless", "Black", DateTime.Today);
+        private Robot robot = new Robot("5", 59.9);
+
+
+        /*
+         * Для числа был рандомайзер, но он создает псевдослучайные числа, отсюда возникли коллизии
+         * когда два числа были одинаковые (притом часто такое встречалось)
+         * Видоизменил на статическое поле с суммированием justANumber
+         */
+        public void DoMagic()
+        {
+            vector.Add(new Vector(10, 10));
+            segment.Start.Add(new Vector(20, 20));
+            blackCat.Rename("Kitty");
+            Robot.BatteryCapacity = justANumber;
+            massiveBytes[2] = Convert.ToByte(justANumber);
+            justANumber += 3;
+        }
+        // Чтобы класс одновременно реализовывал интерфейсы IFactory<A> и IFactory<B> 
+		// придется воспользоваться так называемой явной реализацией интерфейса.
+		// Чтобы отличать методы создания A и B у каждого метода Create нужно явно указать, к какому интерфейсу он относится.
+		// На самом деле такое вы уже видели, когда реализовывали IEnumerable<T>.
+
+		Document IFactory<Document>.Create()
+        {
+            return document;
+        }
+
+		Vector IFactory<Vector>.Create()
+        {
+            return vector;
+        }
+
+        Segment IFactory<Segment>.Create()
+        {
+            return segment;
+        }
+
+        Cat IFactory<Cat>.Create()
+        {
+            return blackCat;
+        }
+
+        Robot IFactory<Robot>.Create()
+        {
+            return robot;
+        }
+    }
+}
---
+++ ghosts/GhostsTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace hashes
+{
+	[TestFixture]
+	public class GhostsTest
+	{
+		[Test]
+		public void GhostDocument()
+		{
+			TestGhost<Document, GhostsTask>();
+		}
+
+		[Test]
+		public void GhostVector()
+		{
+			TestGhost<Vector, GhostsTask>();
+		}
+
+		[Test]
+		public void GhostSegment()
+		{
+			TestGhost<Segment, GhostsTask>();
+		}
+
+		[Test]
+		public void GhostCat()
+		{
+			TestGhost<Cat, GhostsTask>();
+		}
+
+		[Test]
+		public void GhostRobot()
+		{
+			TestGhost<Robot, GhostsTask>();
+		}
+        [Test]
+        public void GhostRobot2()
+        {
+            TestGhost<Robot, GhostsTask>();
+        }
+
+        [Test]
+        public void GhostRobotFiveInARow()
+        {
+            TestGhost<Robot, GhostsTask>();
+            TestGhost<Robot, GhostsTask>();
+            TestGhost<Robot, GhostsTask>();
+            TestGhost<Robot, GhostsTask>();
+            TestGhost<Robot, GhostsTask>();
+		}
+
+		private void TestGhost<TItem, TGhostsTask>() where TGhostsTask : IMagic, IFactory<TItem>, new()
+		{
+			TGhostsTask task = new TGhostsTask();
+			var ghostItem = task.Create();
+
+			var set = new HashSet<TItem> { ghostItem };
+			Assert.IsTrue(set.Contains(task.Create()));
+			task.DoMagic();
+			Assert.IsFalse(set.Contains(ghostItem), "ghost should disappear from HashSet after DoMagic()");
+			set.Add(ghostItem);
+			Assert.AreEqual(2, set.Count, "HashSet Add and Count should work incorrectly after DoMagic()");
+		}
+	}
+}
