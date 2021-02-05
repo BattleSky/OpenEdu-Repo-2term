8c39a4995e3a1db5432f3947aaaa690143d31104
Stepan Filippov <28298843+BattleSky@users.noreply.github.com>
2021-02-05 13:12:11 +0300

    DP_Practice2 undone

--- Жадные алгоритмы/DP_Practice1.cs
+++ Жадные алгоритмы/DP_Practice1.cs
@@ -8,17 +8,22 @@ namespace Динамическое_Программирование
     {
         public int Price;
         public int StartTime, FinishTime;
-
         // Для удобства дебага
         public override string ToString()
         {
             return "Price: " + Price + " Start: " + StartTime + " Finish: " + FinishTime;
         }
-    }
+    } 
+    /*
+         Нужно найти такое ближайшее подходящее событие, 
+         которое заканчивается раньше или вовремя 
+         с началом нового события
+         найти оптимальное среди предыдущего и найденного
+         и записать в массивчик
+    */
 
     class DpPractice1
     {
-
         public static int GetOptimalScheduleGain(params Event[] events)
         {
             // добавление fakeBorderEvent позволяет не обрабатывать некоторые граничные случаи
@@ -30,30 +35,19 @@ namespace Динамическое_Программирование
             opt[0] = 0; // нулевым всегда будет fakeBorderEvent
             for (var k = 1; k < events.Length; k++)
             {
-                //выбираем подходящее мероприятие
-                var chosenMeeting = new Event { StartTime = int.MinValue, FinishTime = int.MaxValue, Price = 0 };
-                var chosenFlag = false;
-                var resultPrice = 0;
-
-                foreach (var e in events)
+                var optOfClosestEvent = 0;
+                for (var i = k-1; i >= 1; i--)
                 {
-                    if (e.StartTime >= events[k].FinishTime && e.FinishTime < chosenMeeting.FinishTime)
-                    {
-                        chosenMeeting = e;
-                        chosenFlag = true;
-                    }
+                    if ((events[i].FinishTime >= events[k].StartTime) || (events[i].FinishTime <= 0)) continue;
+                    optOfClosestEvent = opt[i];
+                    break;
                 }
-                //TODO: плак плак, напиши все заново
-                if (chosenFlag || k == 1)
-                    resultPrice = chosenMeeting.Price + events[k].Price;
-
-                opt[k] = Math.Max(opt[k - 1], resultPrice);
+                opt[k] = Math.Max(opt[k - 1], optOfClosestEvent + events[k].Price);
             }
-
             return opt.Last();
         }
 
-        public static void Main()
+        public static void Mainx()
         {
             var result1 = GetOptimalScheduleGain(new Event[0]); //0
             var result2 = GetOptimalScheduleGain(new Event {StartTime = 1, FinishTime = 11, Price = 50}); //50
@@ -70,8 +64,44 @@ namespace Динамическое_Программирование
                 new Event {StartTime = 14, FinishTime = 16, Price = 90},
                 new Event {StartTime = 12, FinishTime = 15, Price = 200}); //280
 
-            Console.WriteLine("Result1 (must be 0): {0}, \nResult2(must be 50): {1}, \nResultOpenEdu (must be 280): {2}, \nResultUlearnMe (must be 300): {3}", result1, result2, resultOpenEdu, resultUlearnMe);
+            Console.WriteLine("Result1 (must be 0): {0}, \nResult2(must be 50): {1}, \nResultOpenEdu (must be 280): {2}, \nResultUlearnMe (must be 300): {3}", 
+                  result1, result2, 
+                resultOpenEdu, resultUlearnMe);
             Console.Read();
         }
     }
+
+    //public static int GetOptimalScheduleGainBRUH(params Event[] events)
+    //{
+    //// добавление fakeBorderEvent позволяет не обрабатывать некоторые граничные случаи
+    //var fakeBorderEvent = new Event { StartTime = int.MinValue, FinishTime = int.MinValue, Price = 0 };
+    //events = events.Concat(new[] { fakeBorderEvent }).OrderBy(e => e.FinishTime).ToArray();
+
+    //// OPT(k) = Max(OPT(k-1), w(k) + OPT(p(k))
+    //var opt = new int[events.Length];
+    //opt[0] = 0; // нулевым всегда будет fakeBorderEvent
+    //for (var k = 1; k < events.Length; k++)
+    //{
+    //    //выбираем подходящее мероприятие
+    //    var chosenMeeting = new Event { StartTime = int.MinValue, FinishTime = int.MaxValue, Price = 0 };
+    //    var chosenFlag = false;
+    //    var resultPrice = 0;
+
+    //    foreach (var e in events)
+    //    {
+    //        if (e.StartTime >= events[k].FinishTime && e.FinishTime < chosenMeeting.FinishTime)
+    //        {
+    //            chosenMeeting = e;
+    //            chosenFlag = true;
+    //        }
+    //    }
+    //    //TODO: плак плак, напиши все заново
+    //    if (chosenFlag || k == 1)
+    //        resultPrice = chosenMeeting.Price + events[k].Price;
+
+    //    opt[k] = Math.Max(opt[k - 1], resultPrice);
+    //}
+
+    //return opt.Last();
+    //}
 }
---
+++ Жадные алгоритмы/DP_Practice2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Жадные_алгоритмы
+{
+    class DP_Practice2
+    {
+        public static void Main()
+        {
+
+            var result1 = LevenshteinDistance("a", "a");
+            var result2 = LevenshteinDistance("a", "aa");
+            var result3 = LevenshteinDistance("кот", "клон");
+            var result4 = LevenshteinDistance("yabx", "abcd");
+            var result5 = LevenshteinDistance("студент", "солдат");
+            var result6 = LevenshteinDistance("жизньпрекраснакогдамногоденег", "жизньпрекрасна");
+            var result7 = LevenshteinDistance("жизньпрекрасна", "жизньпрекраснакогдамногоденег");
+
+
+            Console.WriteLine("Result1 (must be 0): {0}, \nResult2(must be 1): {1}, \nResult3 (must be 2): {2}, \nResult4 (must be 3): {3}, \nResult5 (must be 4): {4}\nResult6(long->short) (must be 15): {5}\nResult7(short->long) (must be 15): {6}",
+                result1, result2, result3, result4, result5, result6, result7);
+            Console.Read();
+        }
+
+        public static int LevenshteinDistance(string first, string second)
+        {
+            var opt = new int[first.Length + 1, second.Length + 1];
+
+            for (var i = 1; i <= first.Length; ++i) opt[i, 0] = opt[i-1, 0] + 1;
+            for (var i = 1; i <= second.Length; ++i) opt[0, i] = opt[0, i-1] + 1;
+
+            for (var i = 1; i <= first.Length; ++i)
+                for (var j = 1; j <= second.Length; ++j)
+                {
+                    if (first[i - 1] == second[j - 1])
+                    {
+                        opt[i, j] = Math.Min(opt[i - 1, j], Math.Min(opt[i, j - 1], opt[i - 1, j - 1]));
+
+                        if (i == first.Length && first.Length < second.Length)
+                            opt[first.Length, second.Length] = opt[i, j] + (second.Length - first.Length);
+                    }
+                    else
+                        opt[i, j] = Math.Min(opt[i - 1, j] + 1, Math.Min(opt[i, j - 1] + 1, opt[i - 1, j - 1] + 1));
+                }
+            return opt[first.Length, second.Length];
+        }
+    }
+}
