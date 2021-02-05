f103e237cb095b77110d5d3823c523867089a919
Stepan Filippov <28298843+BattleSky@users.noreply.github.com>
2021-02-05 13:36:33 +0300

    dp_practice done

--- Жадные алгоритмы/DP_Practice2.cs
+++ Жадные алгоритмы/DP_Practice2.cs
@@ -21,10 +21,11 @@ namespace Жадные_алгоритмы
 
 
             Console.WriteLine("Result1 (must be 0): {0}, \nResult2(must be 1): {1}, \nResult3 (must be 2): {2}, \nResult4 (must be 3): {3}, \nResult5 (must be 4): {4}\nResult6(long->short) (must be 15): {5}\nResult7(short->long) (must be 15): {6}",
-                result1, result2, result3, result4, result5, result6, result7);
+                result1, 
+                result2, 
+                result3, result4, result5, result6, result7);
             Console.Read();
         }
-
         public static int LevenshteinDistance(string first, string second)
         {
             var opt = new int[first.Length + 1, second.Length + 1];
@@ -32,18 +33,16 @@ namespace Жадные_алгоритмы
             for (var i = 1; i <= first.Length; ++i) opt[i, 0] = opt[i-1, 0] + 1;
             for (var i = 1; i <= second.Length; ++i) opt[0, i] = opt[0, i-1] + 1;
 
-            for (var i = 1; i <= first.Length; ++i)
-                for (var j = 1; j <= second.Length; ++j)
+            for (var i = 1; i <= first.Length; i++)
+                for (var j = 1; j <= second.Length; j++)
                 {
-                    if (first[i - 1] == second[j - 1])
-                    {
-                        opt[i, j] = Math.Min(opt[i - 1, j], Math.Min(opt[i, j - 1], opt[i - 1, j - 1]));
+                    int cost = (second[j - 1] == first[i - 1]) ? 0 : 1;
 
-                        if (i == first.Length && first.Length < second.Length)
-                            opt[first.Length, second.Length] = opt[i, j] + (second.Length - first.Length);
-                    }
-                    else
-                        opt[i, j] = Math.Min(opt[i - 1, j] + 1, Math.Min(opt[i, j - 1] + 1, opt[i - 1, j - 1] + 1));
+                    opt[i, j] = Math.Min(
+                        Math.Min(
+                            opt[i - 1, j] + 1, 
+                        opt[i, j - 1] + 1),
+                        opt[i - 1, j - 1] + cost);
                 }
             return opt[first.Length, second.Length];
         }
